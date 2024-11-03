cbf852da95e90dc5c24ea6eecabaa85e6913e9a9
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-11-03 21:00:10 +0200

    Implemented actions: - use card attack - get card position - use card ability - use hero attack - use hero ability - get frozen cards

--- src/main/Game.java
+++ src/main/Game.java
@@ -8,6 +8,7 @@ import fileio.ActionsInput;
 import fileio.CardInput;
 import fileio.GameInput;
 
+import javax.smartcardio.Card;
 import java.util.ArrayList;
 
 public final class Game {
@@ -100,6 +101,19 @@ public final class Game {
             commandNode.set("output", handArray);
             output.add(commandNode);
         } else if (action.getCommand().equals("endPlayerTurn")) {
+            // verific daca exista carti frozen si le schimb flag-ul
+            int playerIdx = getPlayerTurn();
+            Player.defrostCards(playerIdx, table);
+
+            // le resetez flag-ul cartilor care au atacat tura aceasta
+            Player.rechargeCardsAttack(table);
+
+            if (getPlayerTurn() == 1) {
+                playerOne.getHero().setAttackedThisTurn(false);
+            } else if (getPlayerTurn() == 2) {
+                playerTwo.getHero().setAttackedThisTurn(false);
+            }
+
             // incrementez numarul de ture jucate
             increaseTurns();
 
@@ -120,26 +134,25 @@ public final class Game {
                 playerOne.addCardToHand();
                 playerTwo.addCardToHand();
 
-                if (playerOne.getPlayerMana() < 10) {
-                    playerOne.setPlayerMana(playerOne.getPlayerMana() + (getTurns() / 2) + 1);
+                int manaRound = getTurns() / 2 + 1;
+                if (manaRound > 10) {
+                    manaRound = 10;
                 }
-                if (playerTwo.getPlayerMana() < 10) {
-                    playerTwo.setPlayerMana(playerTwo.getPlayerMana() + (getTurns() / 2) + 1);
-                }
+                playerOne.setPlayerMana(playerOne.getPlayerMana() + manaRound);
+                playerTwo.setPlayerMana(playerTwo.getPlayerMana() + manaRound);
             }
-            /// trebuie sa mai verific daca exista carti frozen si sa le schimb flag-ul
         } else if (action.getCommand().equals("placeCard")) {
             String error;
             if (getPlayerTurn() == 1) {
                 CardInput cardFromHand = playerOne.getHand().get(action.getHandIdx());
-                error = table.addCardToRow(playerOne, cardFromHand);
+                error = table.addCardToTable(playerOne, cardFromHand);
                 if (error == null) {
                     playerOne.setPlayerMana(playerOne.getPlayerMana() - cardFromHand.getMana());
                     playerOne.getHand().remove(action.getHandIdx());
                 }
             } else {
                 CardInput cardFromHand = playerTwo.getHand().get(action.getHandIdx());
-                error = table.addCardToRow(playerTwo, cardFromHand);
+                error = table.addCardToTable(playerTwo, cardFromHand);
                 if (error == null) {
                     playerTwo.setPlayerMana(playerTwo.getPlayerMana() - cardFromHand.getMana());
                     playerTwo.getHand().remove(action.getHandIdx());
@@ -175,6 +188,328 @@ public final class Game {
                 cardsOnTable.add(rowNode);
             }
             commandNode.set("output", cardsOnTable);
+            output.add(commandNode);
+        } else if (action.getCommand().equals("cardUsesAttack")) {
+            int attackerRow = action.getCardAttacker().getX();
+            int attackerColumn = action.getCardAttacker().getY();
+            CardInput attackerCard = table.getRow(attackerRow).get(attackerColumn);
+
+            int attackedRow = action.getCardAttacked().getX();
+            int attackedColumn = action.getCardAttacked().getY();
+            CardInput attackedCard = table.getRow(attackedRow).get(attackedColumn);
+
+            String error = null;
+
+            if (getPlayerTurn() == 1 && action.getCardAttacked().getX() > 1) {
+                error = "Attacked card does not belong to the enemy.";
+            } else if (getPlayerTurn() == 2 && action.getCardAttacked().getX() < 2) {
+                error = "Attacked card does not belong to the enemy.";
+            } else if (attackerCard.isFrozen()) {
+                error = "Attacker card is frozen.";
+            }else if (attackerCard.hasAttackedThisTurn()) {
+                error = "Attacker card has already attacked this turn.";
+            }else if (!attackedCard.isTank()) {
+                if (getPlayerTurn() == 1) {
+                    // randul din fata al adversarului
+                    // acolo se pun tank urile
+                    for (CardInput card : table.getRow(1)) {
+                        if (card.isTank()) {
+                            error = "Attacked card is not of type 'Tank'.";
+                            break;
+                        }
+                    }
+                } else if (getPlayerTurn() == 2) {
+                    // randul din fata al adversarului
+                    // acolo se pun tank urile
+                    for (CardInput card : table.getRow(2)) {
+                        if (card.isTank()) {
+                            error = "Attacked card is not of type 'Tank’.";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (error == null) {
+                attackedCard.setHealth(attackedCard.getHealth() - attackerCard.getAttackDamage());
+                if (attackedCard.getHealth() <= 0) {
+                    table.removeCardFromTable(attackedRow, attackedColumn);
+                }
+                attackerCard.setAttackedThisTurn(true);
+            } else {
+                ObjectNode coordinatesAttacker = objectMapper.createObjectNode();
+                coordinatesAttacker.put("x", action.getCardAttacker().getX());
+                coordinatesAttacker.put("y", action.getCardAttacker().getY());
+
+                ObjectNode coordinatesAttacked = objectMapper.createObjectNode();
+                coordinatesAttacked.put("x", action.getCardAttacked().getX());
+                coordinatesAttacked.put("y", action.getCardAttacked().getY());
+
+                commandNode.put("command", action.getCommand());
+                commandNode.set("cardAttacker", coordinatesAttacker);
+                commandNode.set("cardAttacked", coordinatesAttacked);
+                commandNode.put("error", error);
+                output.add(commandNode);
+            }
+        } else if (action.getCommand().equals("getCardAtPosition")) {
+            commandNode.put("command", action.getCommand());
+            commandNode.put("x", action.getX());
+            commandNode.put("y", action.getY());
+
+            String error = null;
+            if (table.getRow(action.getX()).size() <= action.getY()) {
+                error = "No card available at that position.";
+                commandNode.put("output", error);
+            } else {
+                CardInput card = table.getRow(action.getX()).get(action.getY());
+                ObjectNode cardNode = createCardNode(card, objectMapper);
+                commandNode.set("output", cardNode);
+            }
+            output.add(commandNode);
+        } else if (action.getCommand().equals("cardUsesAbility")) {
+            int attackerRow = action.getCardAttacker().getX();
+            int attackerColumn = action.getCardAttacker().getY();
+            CardInput attackerCard = table.getRow(attackerRow).get(attackerColumn);
+
+            int attackedRow = action.getCardAttacked().getX();
+            int attackedColumn = action.getCardAttacked().getY();
+            CardInput attackedCard = table.getRow(attackedRow).get(attackedColumn);
+
+            String error = null;
+            if (attackerCard.isFrozen()) {
+                error = "Attacker card is frozen.";
+            } else if (attackerCard.hasAttackedThisTurn()) {
+                error = "Attacker card has already attacked this turn.";
+            } else if (attackerCard.getName().equals("Disciple")) {
+                if (getPlayerTurn() == 1 && action.getCardAttacked().getX() < 2) {
+                    error = "Attacked card does not belong to the current player.";
+                } else if (getPlayerTurn() == 2 && action.getCardAttacked().getX() > 1) {
+                    error = "Attacked card does not belong to the current player.";
+                }
+            } else {
+                if (getPlayerTurn() == 1 && action.getCardAttacked().getX() > 1) {
+                    error = "Attacked card does not belong to the enemy.";
+                } else if (getPlayerTurn() == 2 && action.getCardAttacked().getX() < 2) {
+                    error = "Attacked card does not belong to the enemy.";
+                } else if (!attackedCard.isTank()) {
+                    if (getPlayerTurn() == 1) {
+                        // randul din fata al adversarului
+                        // acolo se pun tank urile
+                        for (CardInput card : table.getRow(1)) {
+                            if (card.isTank()) {
+                                error = "Attacked card is not of type 'Tank'.";
+                                break;
+                            }
+                        }
+                    } else if (getPlayerTurn() == 2) {
+                        // randul din fata al adversarului
+                        // acolo se pun tank urile
+                        for (CardInput card : table.getRow(2)) {
+                            if (card.isTank()) {
+                                error = "Attacked card is not of type 'Tank'.";
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (error == null) {
+                if (attackerCard.getName().equals("Disciple")) {
+                    attackedCard.setHealth(attackedCard.getHealth() + 2);
+
+                    attackerCard.setAttackedThisTurn(true);
+                } else if (attackerCard.getName().equals("Miraj")) {
+                    int healthAttacker = attackerCard.getHealth();
+                    int healthAttacked = attackedCard.getHealth();
+
+                    attackerCard.setHealth(healthAttacked);
+                    attackedCard.setHealth(healthAttacker);
+
+                    attackerCard.setAttackedThisTurn(true);
+                } else if (attackerCard.getName().equals("The Ripper")) {
+                    int attackDamageAttacked = attackedCard.getAttackDamage();
+                    attackedCard.setAttackDamage(attackDamageAttacked - 2);
+                    if (attackedCard.getAttackDamage() <= 0) {
+                        attackedCard.setAttackDamage(0);
+                    }
+
+                    attackerCard.setAttackedThisTurn(true);
+                } else if (attackerCard.getName().equals("The Cursed One")) {
+                    int healthAttacked = attackedCard.getHealth();
+                    int attackDamageAttacked = attackedCard.getAttackDamage();
+
+                    attackedCard.setHealth(attackDamageAttacked);
+                    attackedCard.setAttackDamage(healthAttacked);
+
+                    if (attackedCard.getHealth() == 0) {
+                        table.removeCardFromTable(attackedRow, attackedColumn);
+                    }
+
+                    attackerCard.setAttackedThisTurn(true);
+                }
+            } else {
+                ObjectNode coordinatesAttacker = objectMapper.createObjectNode();
+                coordinatesAttacker.put("x", action.getCardAttacker().getX());
+                coordinatesAttacker.put("y", action.getCardAttacker().getY());
+
+                ObjectNode coordinatesAttacked = objectMapper.createObjectNode();
+                coordinatesAttacked.put("x", action.getCardAttacked().getX());
+                coordinatesAttacked.put("y", action.getCardAttacked().getY());
+
+                commandNode.put("command", action.getCommand());
+                commandNode.set("cardAttacker", coordinatesAttacker);
+                commandNode.set("cardAttacked", coordinatesAttacked);
+                commandNode.put("error", error);
+                output.add(commandNode);
+            }
+        } else if (action.getCommand().equals("useAttackHero")) {
+            int attackerRow = action.getCardAttacker().getX();
+            int attackerColumn = action.getCardAttacker().getY();
+            CardInput attackerCard = table.getRow(attackerRow).get(attackerColumn);
+
+            String error = null;
+            if (attackerCard.isFrozen()) {
+                error = "Attacker card is frozen.";
+            } else if (attackerCard.hasAttackedThisTurn()) {
+                error = "Attacker card has already attacked this turn.";
+            } else {
+                if (getPlayerTurn() == 1) {
+                    // randul din fata al adversarului
+                    // acolo se pun tank urile
+                    for (CardInput card : table.getRow(1)) {
+                        if (card.isTank()) {
+                            error = "Attacked card is not of type 'Tank'.";
+                            break;
+                        }
+                    }
+                } else if (getPlayerTurn() == 2) {
+                    // randul din fata al adversarului
+                    // acolo se pun tank urile
+                    for (CardInput card : table.getRow(2)) {
+                        if (card.isTank()) {
+                            error = "Attacked card is not of type 'Tank'.";
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (error == null) {
+                int attackDamageAttacker = attackerCard.getAttackDamage();
+
+                if (getPlayerTurn() == 1) {
+                    // daca este tura primului jucator, atunci este atacat eroul jucatorului 2
+                    int heroHealthPlayerTwo = playerTwo.getHero().getHealth();
+                    playerTwo.getHero().setHealth(heroHealthPlayerTwo - attackDamageAttacker);
+
+                    if (playerTwo.getHero().getHealth() <= 0) {
+                        // jucatorul 1 a castigat, eroul este mort
+                        commandNode.put("gameEnded", "Player one killed the enemy hero.");
+                        output.add(commandNode);
+                    }
+                } else if (getPlayerTurn() == 2) {
+                    // daca este tura celui de al doilea jucator, atunci este atacat eroul
+                    // jucatorului 1
+                    int heroHealthPlayerOne = playerOne.getHero().getHealth();
+                    playerOne.getHero().setHealth(heroHealthPlayerOne - attackDamageAttacker);
+
+                    if (playerOne.getHero().getHealth() <= 0) {
+                        // jucatorul 2 a castigat, eroul este mort
+                        commandNode.put("gameEnded", "Player two killed the enemy hero.");
+                        output.add(commandNode);
+                    }
+                }
+                attackerCard.setAttackedThisTurn(true);
+            } else {
+                ObjectNode coordinatesAttacker = objectMapper.createObjectNode();
+                coordinatesAttacker.put("x", action.getCardAttacker().getX());
+                coordinatesAttacker.put("y", action.getCardAttacker().getY());
+
+                commandNode.put("command", action.getCommand());
+                commandNode.set("cardAttacker", coordinatesAttacker);
+                commandNode.put("error", error);
+                output.add(commandNode);
+            }
+        } else if (action.getCommand().equals("useHeroAbility")) {
+            String error = null;
+            Player currentPlayer;
+            if (getPlayerTurn() == 1) {
+                currentPlayer = playerOne;
+            } else {
+                currentPlayer = playerTwo;
+            }
+
+            Hero currentHero = currentPlayer.getHero();
+            if (currentPlayer.getPlayerMana() < currentHero.getMana()) {
+                error = "Not enough mana to use hero's ability.";
+            } else if (currentHero.hasAttackedThisTurn()) {
+                error = "Hero has already attacked this turn.";
+            } else if (currentHero.getName().equals("Lord Royce") ||
+                    currentHero.getName().equals("Empress Thorina")) {
+                // verific daca randul selectat este al adversarului
+                if (getPlayerTurn() == 1 && action.getAffectedRow() > 1) {
+                    error = "Selected row does not belong to the enemy.";
+                } else if (getPlayerTurn() == 2 && action.getAffectedRow() < 2) {
+                    error = "Selected row does not belong to the enemy.";
+                }
+            } else if (currentHero.getName().equals("General Kocioraw") ||
+                    currentHero.getName().equals("King Mudface")) {
+                if (getPlayerTurn() == 1 && action.getAffectedRow() < 2) {
+                    error = "Selected row does not belong to the current player.";
+                } else if (getPlayerTurn() == 2 && action.getAffectedRow() > 1) {
+                    error = "Selected row does not belong to the current player.";
+                }
+            }
+
+            if (error == null) {
+                int rowIndex = action.getAffectedRow();
+                if (currentHero.getName().equals("Lord Royce")) {
+                    for (CardInput card : table.getRow(rowIndex)) {
+                        card.setFrozen(true);
+                    }
+                } else if (currentHero.getName().equals("Empress Thorina")) {
+                    CardInput destroyCard = table.getRow(rowIndex).get(0);
+                    for (CardInput card : table.getRow(rowIndex)) {
+                        if (card.getHealth() > destroyCard.getHealth()) {
+                            destroyCard = card;
+                        }
+                    }
+                    table.removeCardFromTable(rowIndex,
+                            table.getRow(rowIndex).indexOf(destroyCard));
+                } else if (currentHero.getName().equals("General Kocioraw")) {
+                    for (CardInput card : table.getRow(rowIndex)) {
+                        card.setAttackDamage(card.getAttackDamage() + 1);
+                    }
+                } else if (currentHero.getName().equals("King Mudface")) {
+                    for (CardInput card : table.getRow(rowIndex)) {
+                        card.setHealth(card.getHealth() + 1);
+                    }
+                }
+                currentPlayer.setPlayerMana(currentPlayer.getPlayerMana() - currentHero.getMana());
+                currentHero.setAttackedThisTurn(true);
+            } else {
+                commandNode.put("command", action.getCommand());
+                commandNode.put("affectedRow", action.getAffectedRow());
+                commandNode.put("error", error);
+                output.add(commandNode);
+            }
+        } else if (action.getCommand().equals("getFrozenCardsOnTable")) {
+            commandNode.put("command", action.getCommand());
+            ArrayNode frozenCardsOnTable = objectMapper.createArrayNode();
+
+            for (int rowIndex = 0; rowIndex < 4; rowIndex++) {
+                ArrayNode rowNode = objectMapper.createArrayNode();
+                ArrayList<CardInput> row = table.getRow(rowIndex);
+
+                for (CardInput card : row) {
+                    if (card.isFrozen()) {
+                        ObjectNode cardNode = createCardNode(card, objectMapper);
+                        frozenCardsOnTable.add(cardNode);
+                    }
+                }
+            }
+            commandNode.set("output", frozenCardsOnTable);
             output.add(commandNode);
         }
     }
