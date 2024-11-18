f183cbd3ac341edc90b43c8d4dcb3107d94db9de
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-11-18 19:01:12 +0200

    Method for use attack

--- src/main/java/org/poo/main/Action.java
+++ src/main/java/org/poo/main/Action.java
@@ -57,4 +57,53 @@ public class Action {
                                      ActionsInput action, JsonOutput jsonOutput) {
         output.add(jsonOutput.generateOutput(action, player));
     }
+
+    public static void actionUseAttack(ActionsInput action, Table table, int playerTurn,
+                                       ArrayNode output, JsonOutput jsonOutput) {
+        int attackerRow = action.getCardAttacker().getX();
+        int attackerColumn = action.getCardAttacker().getY();
+        GameCard attackerCard = table.getRow(attackerRow).get(attackerColumn);
+
+        int attackedRow = action.getCardAttacked().getX();
+        int attackedColumn = action.getCardAttacked().getY();
+        GameCard attackedCard = table.getRow(attackedRow).get(attackedColumn);
+
+        String error = null;
+
+        if (playerTurn == 1 && action.getCardAttacked().getX() > 1) {
+            error = "Attacked card does not belong to the enemy.";
+        } else if (playerTurn == 2 && action.getCardAttacked().getX() < 2) {
+            error = "Attacked card does not belong to the enemy.";
+        } else if (attackerCard.isFrozen()) {
+            error = "Attacker card is frozen.";
+        } else if (attackerCard.hasAttackedThisTurn()) {
+            error = "Attacker card has already attacked this turn.";
+        } else if (!attackedCard.isTank()) {
+            if (playerTurn == 1) {
+                for (GameCard card : table.getRow(1)) {
+                    if (card.isTank()) {
+                        error = "Attacked card is not of type 'Tank'.";
+                        break;
+                    }
+                }
+            } else if (playerTurn == 2) {
+                for (GameCard card : table.getRow(2)) {
+                    if (card.isTank()) {
+                        error = "Attacked card is not of type 'Tank'.";
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (error == null) {
+            attackedCard.setHealth(attackedCard.getHealth() - attackerCard.getAttackDamage());
+            if (attackedCard.getHealth() <= 0) {
+                table.removeCardFromTable(attackedRow, attackedColumn);
+            }
+            attackerCard.setAttackedThisTurn(true);
+        } else {
+            output.add(jsonOutput.generateOutputCoordinates(action, error));
+        }
+    }
 }
