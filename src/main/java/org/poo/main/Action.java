c0cdb620a41c121e917fd5088160ecd22aed80da
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-11-20 11:56:57 +0200

    Add actions for attack and ability

--- src/main/java/org/poo/main/Action.java
+++ src/main/java/org/poo/main/Action.java
@@ -1,6 +1,8 @@
 package org.poo.main;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.POJONode;
 import org.poo.fileio.ActionsInput;
 
 import java.util.ArrayList;
@@ -68,33 +70,8 @@ public class Action {
         int attackedColumn = action.getCardAttacked().getY();
         GameCard attackedCard = table.getRow(attackedRow).get(attackedColumn);
 
-        String error = null;
-
-        if (playerTurn == 1 && action.getCardAttacked().getX() > 1) {
-            error = "Attacked card does not belong to the enemy.";
-        } else if (playerTurn == 2 && action.getCardAttacked().getX() < 2) {
-            error = "Attacked card does not belong to the enemy.";
-        } else if (attackerCard.isFrozen()) {
-            error = "Attacker card is frozen.";
-        } else if (attackerCard.hasAttackedThisTurn()) {
-            error = "Attacker card has already attacked this turn.";
-        } else if (!attackedCard.isTank()) {
-            if (playerTurn == 1) {
-                for (GameCard card : table.getRow(1)) {
-                    if (card.isTank()) {
-                        error = "Attacked card is not of type 'Tank'.";
-                        break;
-                    }
-                }
-            } else if (playerTurn == 2) {
-                for (GameCard card : table.getRow(2)) {
-                    if (card.isTank()) {
-                        error = "Attacked card is not of type 'Tank'.";
-                        break;
-                    }
-                }
-            }
-        }
+        String error = ErrorHandle.errorUseAttack(attackerCard, attackedCard, playerTurn,
+                                                    action, table);
 
         if (error == null) {
             attackedCard.setHealth(attackedCard.getHealth() - attackerCard.getAttackDamage());
@@ -103,7 +80,90 @@ public class Action {
             }
             attackerCard.setAttackedThisTurn(true);
         } else {
-            output.add(jsonOutput.generateOutputCoordinates(action, error));
+            output.add(jsonOutput.generateErrorCardAttack(action, error));
+        }
+    }
+
+    public static void
+    actionUseAbility(ActionsInput action, Table table, int playerTurn, ArrayNode output,
+                     JsonOutput jsonOutput) {
+        int attackerRow = action.getCardAttacker().getX();
+        int attackerColumn = action.getCardAttacker().getY();
+        GameCard attackerCard = table.getRow(attackerRow).get(attackerColumn);
+
+        int attackedRow = action.getCardAttacked().getX();
+        int attackedColumn = action.getCardAttacked().getY();
+        GameCard attackedCard = table.getRow(attackedRow).get(attackedColumn);
+
+        String error = ErrorHandle.errorUseAbility(attackerCard, attackedCard, playerTurn,
+                                                    action, table);
+
+        if (error == null) {
+            attackerCard.useAbility(attackedCard);
+            if (attackedCard.getHealth() == 0) {
+                table.removeCardFromTable(attackedRow, attackedColumn);
+            }
+            attackerCard.setAttackedThisTurn(true);
+        } else {
+            output.add(jsonOutput.generateErrorCardAttack(action, error));
+        }
+    }
+
+    public static void
+    actionHeroUseAttack(ActionsInput action, Table table, Player playerOne, Player playerTwo,
+                        int playerTurn, ArrayNode output, GamesStats gamesStats,
+                        JsonOutput jsonOutput) {
+        int attackerRow = action.getCardAttacker().getX();
+        int attackerColumn = action.getCardAttacker().getY();
+        GameCard attackerCard = table.getRow(attackerRow).get(attackerColumn);
+
+        String error = ErrorHandle.errorHeroUseAttack(attackerCard, table, playerTurn);
+
+        if (error == null) {
+            int attackDamageAttacker = attackerCard.getAttackDamage();
+            int playerKilled = 0;
+
+            if (playerTurn == 1) {
+                int heroHealthPlayerTwo = playerTwo.getHero().getHealth();
+                playerTwo.getHero().setHealth(heroHealthPlayerTwo - attackDamageAttacker);
+
+                if (playerTwo.getHero().getHealth() <= 0) {
+                    gamesStats.incrementWinsPlayerOne();
+                    playerKilled = 1;
+                }
+            } else if (playerTurn == 2) {
+                int heroHealthPlayerOne = playerOne.getHero().getHealth();
+                playerOne.getHero().setHealth(heroHealthPlayerOne - attackDamageAttacker);
+
+                if (playerOne.getHero().getHealth() <= 0) {
+                    gamesStats.incrementWinsPlayerTwo();
+                    playerKilled = 2;
+                }
+            }
+            if (playerKilled != 0) {
+                output.add(jsonOutput.generateOutputGameStats(playerKilled));
+            }
+            attackerCard.setAttackedThisTurn(true);
+        } else {
+            output.add(jsonOutput.generateOutputErrorHeroAttack(action, error));
+        }
+    }
+
+    public static void
+    actionHeroUseAbility(Player currentPlayer, ActionsInput action, int playerTurn, Table table,
+                         ArrayNode output, JsonOutput jsonOutput) {
+        String error = null;
+        Hero currentHero = currentPlayer.getHero();
+        error = ErrorHandle.errorHeroUseAbility(currentPlayer, currentHero, action, playerTurn);
+
+        if (error == null) {
+            int rowIndex = action.getAffectedRow();
+            currentHero.useAbility(rowIndex, table);
+
+            currentPlayer.setPlayerMana(currentPlayer.getPlayerMana() - currentHero.getMana());
+            currentHero.setAttackedThisTurn(true);
+        } else {
+            output.add(jsonOutput.generateErrorAffectedRow(action, error));
         }
     }
 }
