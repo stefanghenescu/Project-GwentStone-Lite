5cfdd9d0f62973f79d40da2ed2680cf64865c816
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-11-23 16:03:34 +0200

    Add javadoc and repair coding style

--- src/main/java/org/poo/cardType/Disciple.java
+++ src/main/java/org/poo/cardType/Disciple.java
@@ -2,17 +2,35 @@ package org.poo.cardType;
 import org.poo.fileio.CardInput;
 import org.poo.main.GameCard;
 
-public class Disciple extends Minion {
-    public Disciple(CardInput cardInput) {
+/**
+ * This class represents a Disciple card game
+ * This class is immutable and extends the {@link Minion} class
+ */
+public final class Disciple extends Minion {
+    /**
+     *  As Disciple extends Minion class, and we call super to initialize the
+     *  attributes of Minion class
+     * @param cardInput the card input
+     */
+    public Disciple(final CardInput cardInput) {
         super(cardInput);
     }
 
+    /**
+     * Verify if the card is a tank
+     * @return false as Disciple is not a tank
+     */
     @Override
     public boolean isTank() {
         return false;
     }
 
-    public void useAbility(GameCard attackedCard) {
+    /**
+     * Implement the ability of the Disciple card as this method is ovverriden in every type of card
+     * @param attackedCard the card that will be attacked by the Disciple card
+     */
+    @Override
+    public void useAbility(final GameCard attackedCard) {
         attackedCard.setHealth(attackedCard.getHealth() + 2);
     }
 }
