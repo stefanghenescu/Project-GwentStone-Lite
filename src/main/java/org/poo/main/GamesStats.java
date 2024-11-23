648598cd8f6ceb43cbd01649e7bb9666bbe502f0
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-11-23 18:23:35 +0200

    Add javadocs and repair coding style

--- src/main/java/org/poo/main/GamesStats.java
+++ src/main/java/org/poo/main/GamesStats.java
@@ -7,22 +7,40 @@ public class GamesStats {
     public GamesStats() {
     }
 
+    /**
+     * Method to get the number of wins of player one
+     * @return the number of wins of player one
+     */
     public int getWinsPlayerOne() {
         return winsPlayerOne;
     }
 
+    /**
+     * Method to increment the number of wins of player one
+     */
     public void incrementWinsPlayerOne() {
         winsPlayerOne++;
     }
 
+    /**
+     * Method to get the number of wins of player two
+     * @return the number of wins of player two
+     */
     public int getWinsPlayerTwo() {
         return winsPlayerTwo;
     }
 
+    /**
+     * Method to increment the number of wins of player two
+     */
     public void incrementWinsPlayerTwo() {
         winsPlayerTwo++;
     }
 
+    /**
+     * Method to get the number of games played
+     * @return the number of games played
+     */
     public int getGamesPlayed() {
         return winsPlayerOne + winsPlayerTwo;
     }
