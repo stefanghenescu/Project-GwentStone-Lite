1a5f4e7f7f430f1d7dde9f37e030262064fb2abb
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-11-24 12:37:16 +0200

    Add javadocs and refactor class constructor

--- src/main/java/org/poo/main/Player.java
+++ src/main/java/org/poo/main/Player.java
@@ -2,90 +2,95 @@ package org.poo.main;
 
 import org.poo.cardType.*;
 import org.poo.fileio.*;
-import org.poo.cardType.Hero;
-
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
 public final class Player {
     private int index;
-    private int shuffleSeed;
     private ArrayList<GameCard> deck;
     private Hero hero;
     private ArrayList<GameCard> hand;
     private int playerMana;
 
-    public Player() {
-        hand = new ArrayList<>();
-        playerMana = 1;
+    /**
+     * Constructor for the Player class
+     * @param index the index of the player
+     * @param shuffleSeed the seed for the shuffle of the deck
+     * @param deckInput the input for the deck
+     * @param heroInput the input for the hero
+     */
+    public Player(final int index, final int shuffleSeed, final ArrayList<CardInput> deckInput,
+                      final CardInput heroInput) {
+        this.index = index;
+        this.hand = new ArrayList<>();
+        this.playerMana = 1;
+
+        // Initialize the deck
+        this.deck = new ArrayList<>();
+        for (CardInput card : deckInput) {
+            String cardType = card.getName();
+            switch (cardType) {
+                case "The Ripper":
+                    this.deck.add(new TheRipper(card));
+                    break;
+                case "Miraj":
+                    this.deck.add(new Miraj(card));
+                    break;
+                case "The Cursed One":
+                    this.deck.add(new TheCursedOne(card));
+                    break;
+                case "Disciple":
+                    this.deck.add(new Disciple(card));
+                    break;
+                case "Goliath":
+                    this.deck.add(new Goliath(card));
+                    break;
+                case "Warden":
+                    this.deck.add(new Warden(card));
+                    break;
+                default:
+                    this.deck.add(new Minion(card));
+            }
+        }
+
+        // Shuffle the deck
+        Random random = new Random(shuffleSeed);
+        Collections.shuffle(this.deck, random);
+
+        // Initialize the hero
+        switch (heroInput.getName()) {
+            case "Lord Royce":
+                this.hero = new LordRoyce(heroInput);
+                break;
+            case "Empress Thorina":
+                this.hero = new EmpressThorina(heroInput);
+                break;
+            case "King Mudface":
+                this.hero = new KingMudface(heroInput);
+                break;
+            case "General Kocioraw":
+                this.hero = new GeneralKocioraw(heroInput);
+                break;
+            default:
+                this.hero = new Hero(heroInput);
+        }
     }
 
     public int getIndex() {
         return index;
     }
 
-    public void setIndex(final int index) {
-        this.index = index;
-    }
-
-    public int getShuffleSeed() {
-        return shuffleSeed;
-    }
-
-    public void setShuffleSeed(final int shuffleSeed) {
-        this.shuffleSeed = shuffleSeed;
-    }
-
     public ArrayList<GameCard> getDeck() {
         return deck;
-    }
-
-    public void setDeck(final ArrayList<CardInput> deck) {
-        ArrayList<GameCard> copyDeck = new ArrayList<>();
-        for (CardInput card : deck) {
-            String cardType = card.getName();
-            switch (cardType) {
-                case "The Ripper":
-                    copyDeck.add(new TheRipper(card));
-                    break;
-                case "Miraj":
-                    copyDeck.add(new Miraj(card));
-                    break;
-                case "The Cursed One":
-                    copyDeck.add(new TheCursedOne(card));
-                    break;
-                case "Disciple":
-                    copyDeck.add(new Disciple(card));
-                    break;
-                case "Goliath":
-                    copyDeck.add(new Goliath(card));
-                    break;
-                case "Warden":
-                    copyDeck.add(new Warden(card));
-                    break;
-                default:
-                    copyDeck.add(new Minion(card));
-            }
-        }
-        this.deck = copyDeck;
     }
 
     public Hero getHero() {
         return hero;
     }
 
-    public void setHero(final Hero hero) {
-        this.hero = hero;
-    }
-
     public ArrayList<GameCard> getHand() {
         return hand;
-    }
-
-    public void setHand(final ArrayList<GameCard> hand) {
-        this.hand = hand;
     }
 
     public int getPlayerMana() {
@@ -96,97 +101,16 @@ public final class Player {
         this.playerMana = playerMana;
     }
 
+    /**
+     * Method to draw a card from the deck and add it to the hand
+     */
     public void addCardToHand() {
         if (!deck.isEmpty()) {
-            // adaugam o carte din deck in mana
-            this.getHand().add(this.getDeck().get(0));
-            // scoatem o din deck
-            this.getDeck().remove(0);
+            // Add a card from the deck to the hand
+            this.getHand().add(this.getDeck().getFirst());
+
+            // Remove the card from the deck
+            this.getDeck().removeFirst();
         }
     }
-
-    public static Player initializePlayerOne(GameInput game, Input inputData) {
-        Player playerOne = new Player();
-        playerOne.setIndex(1);
-
-        StartGameInput startGame = game.getStartGame();
-
-        // shuffleSeed-ul fiecarui joc
-        playerOne.setShuffleSeed(startGame.getShuffleSeed());
-
-        // la fiecare meci selectam deck-ul cu care va juca fiecare jucator
-        int indexDeckPlayerOne = startGame.getPlayerOneDeckIdx();
-        playerOne.setDeck(inputData.getPlayerOneDecks().getDecks().get(indexDeckPlayerOne));
-
-        // se amesteca deck-urile
-        Random randomPlayerOne = new Random(playerOne.getShuffleSeed());
-        Collections.shuffle(playerOne.getDeck(), randomPlayerOne);
-
-        // fiecare jucator primeste un erou
-        String heroName = startGame.getPlayerOneHero().getName();
-        CardInput heroCard = startGame.getPlayerOneHero();
-        Hero heroPlayerOne;
-        switch (heroName) {
-            case "Lord Royce":
-                heroPlayerOne = new LordRoyce(heroCard);
-                break;
-            case "Empress Thorina":
-                heroPlayerOne = new EmpressThorina(heroCard);
-                break;
-            case "King Mudface":
-                heroPlayerOne = new KingMudface(heroCard);
-                break;
-            case "General Kocioraw":
-                heroPlayerOne=  new GeneralKocioraw(heroCard);
-                break;
-            default:
-                heroPlayerOne = new Hero(heroCard);
-        }
-
-        playerOne.setHero(heroPlayerOne);
-        return playerOne;
-    }
-
-    public static Player initializePlayerTwo(GameInput game, Input inputData) {
-        Player playerTwo = new Player();
-        playerTwo.setIndex(2);
-
-        StartGameInput startGame = game.getStartGame();
-
-        // shuffleSeed-ul fiecarui joc
-        playerTwo.setShuffleSeed(startGame.getShuffleSeed());
-
-        // la fiecare meci selectam deck-ul cu care va juca fiecare jucator
-        int indexDeckPlayerTwo = startGame.getPlayerTwoDeckIdx();
-        playerTwo.setDeck(inputData.getPlayerTwoDecks().getDecks().get(indexDeckPlayerTwo));
-
-        // se amesteca deck-urile
-        Random randomPlayerTwo = new Random(playerTwo.getShuffleSeed());
-        Collections.shuffle(playerTwo.getDeck(), randomPlayerTwo);
-
-        // fiecare jucator primeste un erou
-        String heroName = startGame.getPlayerTwoHero().getName();
-        CardInput heroCard = startGame.getPlayerTwoHero();
-        Hero heroPlayerTwo;
-        switch (heroName) {
-            case "Lord Royce":
-                heroPlayerTwo = new LordRoyce(heroCard);
-                break;
-            case "Empress Thorina":
-                heroPlayerTwo = new EmpressThorina(heroCard);
-                break;
-            case "King Mudface":
-                heroPlayerTwo = new KingMudface(heroCard);
-                break;
-            case "General Kocioraw":
-                heroPlayerTwo=  new GeneralKocioraw(heroCard);
-                break;
-            default:
-                heroPlayerTwo = new Hero(heroCard);
-        }
-
-        playerTwo.setHero(heroPlayerTwo);
-        return playerTwo;
-    }
-
 }
